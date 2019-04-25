6df2da152fd8fd4cacb40c62c9fd2a0fe401c77f
Adam Uhlir <uhlir.a@gmail.com>
2019-04-25 09:31:22 -0700

    Tests for lock.js and version.js
    License: MIT
    Signed-off-by: Adam Uhlir <uhlir.a@gmail.com>

--- test/browser.js
+++ test/browser.js
@@ -1,5 +1,28 @@
 /* eslint-env mocha */
-
 'use strict'
 
+const Datastore = require('datastore-level')
 
+async function repoSetup () {
+  const date = Date.now().toString()
+  const dir = 'test-repo-for-' + date
+  const store = new Datastore(dir, { extension: '', createIfMissing: true })
+  await store.open()
+  await store.close()
+  return dir
+}
+
+async function repoCleanup (dir) {
+}
+
+describe('Browser specific tests', () => {
+  describe('lock.js tests', () => {
+    describe('mem-lock tests', () => {
+      require('./lock-test')(require('../src/repo/lock-memory'), repoSetup, repoCleanup)
+    })
+  })
+
+  describe('version tests', () => {
+    require('./version-test')(repoSetup, repoCleanup)
+  })
+})
--- test/node.js
+++ test/node.js
@@ -1,8 +1,37 @@
 /* eslint-env mocha */
 'use strict'
 
+const rimraf = require('rimraf')
+const promisify = require('util').promisify
+const asyncRimraf = promisify(rimraf)
+const path = require('path')
+const fs = require('fs')
 
+async function repoSetup () {
+  const testRepoPath = path.join(__dirname, 'test-repo')
+  const date = Date.now().toString()
+  const dir = testRepoPath + '-for-' + date
+  fs.mkdirSync(dir)
 
-describe('IPFS Repo Migrations tests on the Node', () => {
+  return dir
+}
 
+async function repoCleanup (dir) {
+  return asyncRimraf(dir)
+}
+
+describe('Node specific tests', () => {
+  describe('lock.js tests', () => {
+    describe('fs-lock tests', () => {
+      require('./lock-test')(require('../src/repo/lock'), repoSetup, repoCleanup)
+    })
+
+    describe('mem-lock tests', () => {
+      require('./lock-test')(require('../src/repo/lock-memory'), repoSetup, repoCleanup)
+    })
+  })
+
+  describe('version tests', () => {
+    require('./version-test')(repoSetup, repoCleanup)
+  })
 })
