e5a6ab077f1cad6f6fc5aef17c0d3293a46d7e8f
achingbrain <alex@achingbrain.net>
2021-08-17 15:47:05 +0100

    chore: await close and open

--- migrations/migration-11/index.js
+++ migrations/migration-11/index.js
@@ -11,15 +11,15 @@ const MFS_ROOT_KEY = new Key('/local/filesroot')
 async function storeMfsRootInDatastore (backends, onProgress = () => {}) {
   onProgress(100, 'Migrating MFS root to repo datastore')
 
-  backends.root.open()
-  backends.datastore.open()
+  await backends.root.open()
+  await backends.datastore.open()
 
   const root = await backends.root.get(MFS_ROOT_KEY)
   await backends.datastore.put(MFS_ROOT_KEY, root)
   await backends.root.delete(MFS_ROOT_KEY)
 
-  backends.datastore.close()
-  backends.root.close()
+  await backends.datastore.close()
+  await backends.root.close()
 
   onProgress(100, 'Stored MFS root in repo datastore')
 }
@@ -31,15 +31,15 @@ async function storeMfsRootInDatastore (backends, onProgress = () => {}) {
  async function storeMfsRootInRoot (backends, onProgress = () => {}) {
   onProgress(100, 'Migrating MFS root to repo root datastore')
 
-  backends.root.open()
-  backends.datastore.open()
+  await backends.root.open()
+  await backends.datastore.open()
 
   const root = await backends.datastore.get(MFS_ROOT_KEY)
   await backends.root.put(MFS_ROOT_KEY, root)
   await backends.datastore.delete(MFS_ROOT_KEY)
 
-  backends.datastore.close()
-  backends.root.close()
+  await backends.datastore.close()
+  await backends.root.close()
 
   onProgress(100, 'Stored MFS root in repo root datastore')
 }
